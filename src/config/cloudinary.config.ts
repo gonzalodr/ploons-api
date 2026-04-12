03f9aa6ba7517b29324e1926e788f276739caf9a
gonzalodr <gonzalodormos26@gmail.com>
2026-04-12 13:32:17 -0600

    fix: fix index and cloudinary config

--- src/config/cloudinary.config.ts
+++ src/config/cloudinary.config.ts
@@ -7,7 +7,7 @@ const cloud_name = process.env.CLOUDINARY_CLOUD_NAME;
 const api_key = process.env.CLOUDINARY_API_KEY;
 const api_secret = process.env.CLOUDINARY_API_SECRET;
 
-if (!cloud_name || !api_key || api_secret) {
+if (!cloud_name || !api_key || !api_secret) {
   throw new Error("Missing CLAUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY or CLOUDINARY_API_SECRET in environment variables");
 }
 
--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,18 @@ import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from '@docs/swagger.json';
 import { prisma } from '@config/db.config';
 
-const app = express();
 const PORT = process.env.PORT || 4000;
 const ENVIRONMENT = process.env.ENVIRONMENT || 'development';
-const FRONTEND_URL = ENVIRONMENT === 'production' ? 'http://192.168.137.1:3000' : process.env.FRONTEND_URL || '*';
+const FRONTEND_URL = ENVIRONMENT === 'production' ? process.env.FRONTEND_URL : 'http://127.0.0.1:3000';
 
 if (ENVIRONMENT === 'production' && !FRONTEND_URL) {
   throw new Error('FRONTEND_URL is required in production');
 }
 
+const app = express();
 // 1. cors config
 app.use(cors({
-  origin: '*',
+  origin: FRONTEND_URL,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'X-Refresh-Token']
@@ -50,8 +50,8 @@ app.use('/save', savedRouter);
 app.use('/search', searchRouter);
 app.use('/feed', feedRouter);
 //5.5 handler endpoints not found
-app.use('*', (req, res) => {
-  res.status(404).json({ error: `Route ${req.originalUrl} not found` });
+app.use((req, res) => {
+  res.status(404).json({ error: `Route \`${req.originalUrl}\` not found` });
 });
 // 6. listen ports
 const server = app.listen(Number(PORT), '0.0.0.0', () => {
