8305b38f813b0facbebd76e26133cdc1ee248816
gonzalodr <gonzalodormos26@gmail.com>
2026-04-12 13:33:48 -0600

    fix: fix router profile

--- src/modules/profile/profile.routes.ts
+++ src/modules/profile/profile.routes.ts
@@ -1,14 +1,14 @@
 import { Router } from 'express';
 import { ProfileController } from 'src/modules/profile/profile.controller';
 import { authenticate } from '@middlewares/auth.middleware';
-import { upload } from '@middlewares/multer.middleware';
+import { uploadSingle } from '@middlewares/multer.middleware';
 import { authOptional } from '@middlewares/authOptional.middlewares';
 
 const router = Router();
 const profileController = new ProfileController();
 
 router.get("/me",authenticate,profileController.getMyProfile.bind(profileController));
-router.put("/me",authenticate,upload.single("avatar"),profileController.updateProfile.bind(profileController));
+router.put("/me",authenticate,uploadSingle('avatar') ,profileController.updateProfile.bind(profileController));
 router.get("/:profileId",authOptional,profileController.getProfileById.bind(profileController));
 
 export default router;
